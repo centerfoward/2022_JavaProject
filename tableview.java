defe67ba111d1207007d414bfb5c66e1883e9178
roundbot <118614149+roundbot@users.noreply.github.com>
2022-12-13 22:13:06 +0900

    Update tableview.java

--- tableview.java
+++ tableview.java
@@ -1,29 +1,52 @@
 import java.sql.*; 
-public class tableview { 
-public static void main(String[] args){ 
-String jdbc_url = "jdbc:mysql://localhost:3306/dogbesa?serverTimezone=UTC"; 
-Connection con; Statement stmt; 
-try { 
-Class.forName("com.mysql.cj.jdbc.Driver"); 
-}catch(ClassNotFoundException e) { 
-System.err.println("ClassNotFoundException: " + e.getMessage()); 
-} 
-try{ 
-con = DriverManager.getConnection(jdbc_url, "root", "1234"); 
-stmt = con.createStatement(); 
-String sql ="SELECT * FROM user_info"; 
-ResultSet rs = stmt.executeQuery(sql); 
-System.out.println( "<< 회원 명단 >>");
-System.out.println( "[번호 ]\t" + " [이름]\t" + " [견종]");
-while(rs.next()) { 
-int phoneNumber = rs.getInt("phoneNumber"); 
-String name = rs.getString("name");
-String birthday = rs.getString("birthday");
-System.out.println( phoneNumber + " ," + name +","+birthday); 
-} 
-rs.close(); 
-stmt.close(); 
-con.close(); 
-}catch(SQLException e) { 
-System.err.println("SQLException: " + e.getMessage()); 
-}} }
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class tableview extends JFrame implements ActionListener { 
+	
+  String[] name = { "번호", "이름", "견종"};
+  
+  DefaultTableModel dt = new DefaultTableModel(name, 0);
+  JTable jt = new JTable(dt);
+  JScrollPane jsp = new JScrollPane(jt);
+
+  JPanel panel = new JPanel();
+  String[] comboName = { "  번호  ", "  이름  ", "  견종 "};
+
+  JComboBox combo = new JComboBox(comboName);
+  JTextField jtf = new JTextField(20);
+  JButton serach = new JButton("검색");
+
+  tableview(){
+	  super("회원 명단");
+	  setSize(400, 400);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		panel.setBackground(Color.white);
+        panel.add(combo);
+        panel.add(jtf);
+        panel.add(serach);
+        
+        add(jsp, "Center");
+        add(panel, "South");
+        
+        serach.addActionListener(this);
+        }
+        public void actionPerformed(ActionEvent e) {
+			
+		}
+ 
+
+  public static void main(String[] args){ 
+	  new tableview();
+  }}
