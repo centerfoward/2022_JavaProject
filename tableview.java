686bddc550cfabe27bca24ef249bee0a6d64f58a
jinjin-0 <63450214+jinjin-0@users.noreply.github.com>
2022-12-13 23:46:21 +0900

    Update tableview.java

--- tableview.java
+++ tableview.java
@@ -10,8 +10,11 @@ import java.sql.Statement;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
+
+
 public class tableview extends JFrame implements ActionListener { 
 	
+	 
   String[] name = { "번호", "이름", "견종"};
   
   DefaultTableModel dt = new DefaultTableModel(name, 0);
@@ -22,19 +25,34 @@ public class tableview extends JFrame implements ActionListener {
   String[] comboName = { "  번호  ", "  이름  ", "  견종 "};
 
   JComboBox combo = new JComboBox(comboName);
-  JTextField jtf = new JTextField(20);
+
   JButton serach = new JButton("검색");
+  JTextArea jta = new JTextArea(20,20);
+  public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		String jdbc_url = "jdbc:mysql://localhost:3306/dogbesa?serverTimezone=UTC"; 
+		Connection con; Statement stmt; 
+		
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dogbesa?serverTimezone=UTC", "root",
+				"1234"); //데이터베이스 이름 : dogbesa,   테이블 이름 : user_info, 행 : id, password, name, birthday, phoneNumber, sex, dog
+		
+
+		return conn;
+ 
+  }
 
   tableview(){
 	  super("회원 명단");
+
 	  setSize(400, 400);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		
 		panel.setBackground(Color.white);
-        panel.add(combo);
-        panel.add(jtf);
+
+        panel.add(jta);
         panel.add(serach);
         
         add(jsp, "Center");
@@ -43,9 +61,34 @@ public class tableview extends JFrame implements ActionListener {
         serach.addActionListener(this);
         }
         public void actionPerformed(ActionEvent e) {
-			
+        	String jdbc_url = "jdbc:mysql://localhost:3306/dogbesa?serverTimezone=UTC"; 
+        	Connection con; Statement stmt; 
+        	
+        	try { 
+        	Class.forName("com.mysql.cj.jdbc.Driver"); 
+        	}catch(ClassNotFoundException ss) { 
+        	System.err.println("ClassNotFoundException: " + ss.getMessage()); 
+        	} 
+        	try{ 
+        	con = DriverManager.getConnection(jdbc_url, "root", "1234"); 
+        	stmt = con.createStatement(); 
+        	String sql ="SELECT * FROM user_info"; 
+        	ResultSet rs = stmt.executeQuery(sql); 
+    
+        	while(rs.next()) { 
+        	int phoneNumber = rs.getInt("phoneNumber"); 
+        	String name = rs.getString("name");
+        	String dog = rs.getString("dog");
+        
+        	jta.append("        "+ phoneNumber + "     |     " + name + "     |     " +dog + "\n");
+        	} 
+        	rs.close(); 
+        	stmt.close(); 
+        	con.close(); 
+        	}catch(SQLException ss) { 
+        		System.err.println("SQLException: " + ss.getMessage()); 
+        	}
 		}
- 
 
   public static void main(String[] args){ 
 	  new tableview();
